2cd32d48721bffadb005327a068e8ebe838f4653
KKWANH <kwanho0096@gmail.com>
2020-12-29 18:27:14 +0900

    P1_Player_Moving

--- P1_Player/raycast.js
+++ P1_Player/raycast.js
@@ -42,24 +42,74 @@ class       Map {
 
 class       Player
 {
-    x;
-    y;
-    // radius
-    rad;
-    // turnDirection : -1(left), 1(right), 0(nothing to update) : 실제 각도가 아니라 왼쪽인지 오른쪽인지만.
-    dir_turn = 0;
-    // walkDirection : -1(back), 1(front), 0(nothing to update) : 실제 각도가 아니라 앞인지 뒤인지만.
-    dir_walk = 0;
-    // rotationAngle : PI = 180도, PI/2 = 90도
-    ang_rota = Math.PI / 2;
-    // moveSpeed 움직이는 속도
-    spd_move = 3.0;
-    // rotationSpeed : 3도씩 회전
-    spd_rota = 3 * (MATH.PI / 180);
+    constructor()
+    {
+        this.x = WIND_WID / 2;
+        this.y = WIND_HEI / 2;
+        this.rad = 3;
+        this.dir_turn = 0;                      // turnDirection : -1(left), 1(right), 0(nothing to update) : 실제 각도가 아니라 왼쪽인지 오른쪽인지만.
+        this.dir_walk = 0;                      // walkDirection : -1(back), 1(front), 0(nothing to update) : 실제 각도가 아니라 앞인지 뒤인지만.
+        this.ang_rota = Math.PI / 2;            // rotationAngle : PI = 180도, PI/2 = 90도s
+        this.spd_move = 3.0;                    // moveSpeed 움직이는 속도
+        this.spd_rota = 3 * (Math.PI / 100);    // rotationSpeed : 3도씩 회전
+    }
 
+    // TODO: update player position based on turnDirection and walkDirection
+    update()
+    {
+        this.ang_rota += this.dir_turn * this.spd_rota;
+
+        var moveStep = this.dir_walk * this.spd_move;
+        this.x += Math.cos(this.ang_rota) * moveStep;
+        this.y += Math.sin(this.ang_rota) * moveStep;
+    }
+
+    render()
+    {
+        noStroke();
+        // 위치
+        fill("red");
+        circle(this.x, this.y, this.rad);
+        // 라인
+        stroke("red");
+        line(
+            this.x,
+            this.y,
+            this.x + Math.cos(this.ang_rota) * 30,
+            this.y + Math.sin(this.ang_rota) * 30
+        );
+    }
 }
 
 var         grid = new Map();
+var         player = new Player();
+
+// TODO :
+function    keyPressed()
+{
+    if (keyCode == UP_ARROW) {
+        player.dir_walk = +1;
+    } else if (keyCode == DOWN_ARROW) {
+        player.dir_walk = -1;
+    } else if (keyCode == RIGHT_ARROW) {
+        player.dir_turn = +1;
+    } else if (keyCode == LEFT_ARROW) {
+        player.dir_turn = -1;
+    }
+}
+
+function    keyReleased()
+{
+    if (keyCode == UP_ARROW) {
+        player.dir_walk = 0;
+    } else if (keyCode == DOWN_ARROW) {
+        player.dir_walk = 0;
+    } else if (keyCode == RIGHT_ARROW) {
+        player.dir_turn = 0;
+    } else if (keyCode == LEFT_ARROW) {
+        player.dir_turn = 0;
+    }
+}
 
 // TODO: initialize all objects 모든 오브젝트 초기화하기
 function    setup()
@@ -70,7 +120,7 @@ function    setup()
 // TODO: update all game objects before we render the next frame 다음 프레임을 렌더링 하기 전에 모든 게임 오브젝트를 업데이트
 function    update()
 {
-
+    player.update();
 }
 
 // TODO: render all objects frame by frame 모든 개체를 프레임 별로 렌더링
@@ -80,4 +130,5 @@ function    draw()
     update();
 
     grid.render();
+    player.render();
 }
