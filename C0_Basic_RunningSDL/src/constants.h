f615f3602ab08c055db35b3dade2ce3995f9f697
KKWANH <kwanho0096@gmail.com>
2021-01-02 16:07:06 +0900

    C0_Basic_RunningSDL

---
+++ C0_Basic_RunningSDL/src/constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+# include	<unistd.h>
+# include	<SDL2/SDL.h>
+
+# define	FALSE				0
+# define	TRUE				1
+# define	WINDOW_WIDTH		800
+# define	WINDOW_HEIGHT		600
+
+#endif
